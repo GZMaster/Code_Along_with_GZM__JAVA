a8188ddf37994b04365c21f9555afba5a2edaed4
jesse406 <ismailajesse@gmail.com>
2022-08-13 23:55:24 +0100

    Chapter 1 examples

---
+++ src/main/java/HelloWorldTest/something.java
@@ -0,0 +1,23 @@
+package HelloWorldTest;
+
+public class something {
+   public static void main (String[] args) {
+       String name = "Dirk";
+       int y = 0;
+               int x = 10;
+       if (x == 10) {
+           System.out.print("x must be 10");
+       } else {
+           System.out.print("x isn't 10");
+       }
+       if ((y < 3) & (name.equals("Dirk"))) {
+    System.out.println("Gently");
+}
+System.out.print("this line runs no matter what");
+}
+       }
+
+
+
+
+
