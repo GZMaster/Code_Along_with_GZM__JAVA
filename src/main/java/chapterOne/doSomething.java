62297438f8e1bcf3148ec9335cd8c0dbf208a288
GZMaster <danigamester@gmail.com>
2022-08-14 01:11:02 +0100

    fixing

---
+++ src/main/java/chapterOne/doSomething.java
@@ -0,0 +1,11 @@
+package chapterOne;
+
+public class doSomething {
+    public static void main (String[] args) {
+        int x = 3;
+        String name = "GZM";
+        x = x * 17;
+        System.out.println("x is " + x);
+        double d = Math.random();
+    }
+}
