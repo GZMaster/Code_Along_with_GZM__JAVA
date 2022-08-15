16bd3516083f697a76aab3194dcddeef27bb44a1
GZMaster <danigamester@gmail.com>
2022-08-15 23:12:23 +0100

    Movie and GuessGame

--- src/main/java/Chapter2/Movie.java
+++ src/main/java/Chapter2/Movie.java
@@ -1,7 +1,7 @@
 package Chapter2;
 
 public class Movie {
-    String tile;
+    String title;
     String genre;
     int rating;
 
@@ -10,20 +10,3 @@ public class Movie {
     }
 }
 
-public class MovieTestDrive {
-    public static void main (String[] args) {
-        Movie one = new Movie();
-        one.title = "Gone with the Stock";
-        one.genre = "Tragic";
-        one.rating = -2;
-        Movie two = new Movie ();
-        two.title = "Lost in Cubicle Space";
-        two.genre = "Comedy";
-        two.rating = 5;
-        two.playIt();
-        Movie three = new Movie();
-        three.title = "Byte Club";
-        three.genre = "Tragic but ultimately uplifting";
-        three.ratin = 127;
-    }
-}
